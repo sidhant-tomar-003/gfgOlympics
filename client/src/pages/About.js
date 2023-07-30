923ce5959892d303c7fe736748310aba5397b790
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-07-30 20:50:44 +0530

    Squashed commit of the following:
    commit 587bbcf661d4f05728f37d01404923412ffe0bb2
    Merge: de749d9 20b2701
    Author: Sid-the-Slothh <92656939+Sid-the-Slothh@users.noreply.github.com>
    Date:   Sun Jul 30 20:35:56 2023 +0530
    
        Merge pull request #2 from ThapaVinay/main
    
        added about us and contact page
    
    commit 20b2701e7439c3722d1543da220766e48493b8cb
    Author: lonewolf <thechampthapa@gmail.com>
    Date:   Sun Jul 30 20:22:06 2023 +0530
    
        some fixes
    
    commit 052f78a24f060fcb278735041254864a572873aa
    Author: lonewolf <thechampthapa@gmail.com>
    Date:   Sun Jul 30 19:57:48 2023 +0530
    
        added about us and contact page
    
    commit de749d9c5052d7af13b4b367af8e0aee34d41dd6
    Merge: 67f3df2 8f261a6
    Author: Sid-the-Slothh <92656939+Sid-the-Slothh@users.noreply.github.com>
    Date:   Sat Jul 29 21:20:32 2023 +0530
    
        Merge pull request #1 from H-SM/H-SM
    
        converted to a react application. Good job HSM!

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,17 +1,39 @@
 import React from 'react'
+import image from "./image.jpg";
 
-const About = () => {
-  return (
-    <>
-      <section class="firstSection">
-            <div class="leftSection">Hi this is our <span class="purple">About page</span></div>
-            <div class="rightSection">
+function About() {
+    return (
+        <div style={{ marginTop: "30px" }}>
+            <div className="text-white aboutImg text-center">
+                <div className="note-img me-3">
+                    <h2 className="mb-3 mt-4" style={{ fontWeight: "Bold" , fontSize:"35px"}}>About <span style={{ color: "#9C27B0" }}>Us</span> </h2>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium eligendi voluptatem delectus, ad cumque? Sequi, ullam quas! Consequatur commodi quae, possimus similique repellendus assumenda omnis temporibus amet cupiditate unde ad eum.</p>
+                </div>
             </div>
-        </section>
 
-        <hr/>
-    </>
-  )
+            <div className="container mt-5 ">
+                <div className="row">
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                        <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Why use <span style={{ color: "#9C27B0" }}>our website?</span> </h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit temporibus inventore ullam sunt, exercitationem dignissimos est neque illum, sint molestiae similique quibusdam, blanditiis nemo eius. Dolor assumenda, labore quibusdam, dolore quo illum architecto ipsa, recusandae quasi placeat voluptate sapiente.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <img className="img-fluid" style={{ padding: "30px" }} src={image} alt="olympics" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="d-flex justify-content-center">
+                <hr style={{ width: "500px", marginTop: '50px', height: "1px", backgroundColor: "grey" }} />
+            </div>
+
+            <h2 className="mb-3" style={{ fontWeight: "Bold" , display:"flex", justifyContent:"center", marginTop:"20px"}}>Made <span style={{ color: "#9C27B0" }}>By</span> </h2>
+            <p style={{ display: "flex", justifyContent: "center", fontFamily:"'Source Code Pro', monospace" }}>@EdgeRunners</p>
+
+        </div>
+    )
 }
 
-export default About;
+export default About
