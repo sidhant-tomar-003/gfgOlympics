052f78a24f060fcb278735041254864a572873aa
lonewolf <thechampthapa@gmail.com>
2023-07-30 19:57:48 +0530

    added about us and contact page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,17 +1,38 @@
 import React from 'react'
+import image from "./image.jpg";
 
-const About = () => {
-  return (
-    <>
-      <section class="firstSection">
-            <div class="leftSection">Hi this is our <span class="purple">About page</span></div>
-            <div class="rightSection">
+function About() {
+    return (
+        <div style={{ marginTop: "30px" }}>
+            <div className="text-white aboutImg text-center">
+                <div className="note-img me-3">
+                    <h1 className="display-4">Skellington Website</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium eligendi voluptatem delectus, ad cumque? Sequi, ullam quas! Consequatur commodi quae, possimus similique repellendus assumenda omnis temporibus amet cupiditate unde ad eum.</p>
+                </div>
             </div>
-        </section>
 
-        <hr/>
-    </>
-  )
+            <div className="container mt-5 ">
+                <div className="row">
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                        <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Why use <span style={{ color: "#9C27B0" }}>our website?</span> </h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit temporibus inventore ullam sunt, exercitationem dignissimos est neque illum, sint molestiae similique quibusdam, blanditiis nemo eius. Dolor assumenda, labore quibusdam, dolore quo illum architecto ipsa, recusandae quasi placeat voluptate sapiente.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <img className="img-fluid" style={{padding:"30px"}} src={image} alt="olympics" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="d-flex justify-content-center">
+                <hr style={{ width: "500px", marginTop: '50px', height: "3px", backgroundColor: "grey" }} />
+            </div>
+
+            <h2 style={{ fontWeight: "bold", display: "flex", justifyContent: "center", marginTop: "20px" }}>Made By</h2>
+            <p style={{display:"flex", justifyContent:"center"}}>@Edge Runners</p>
+
+        </div>
+    )
 }
 
-export default About;
+export default About
